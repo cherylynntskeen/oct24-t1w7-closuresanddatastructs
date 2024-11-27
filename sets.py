59e586506580689a9e7e7bf620d5ad875c3a157c
cherylynntskeen <cherylynnskeen@hotmail.com>
2024-11-27 20:07:08 +1000

    Adding python files - closure.py dictionaries.py multiplier.py sets.py

---
+++ sets.py
@@ -0,0 +1,25 @@
+# List - ordered (indexed) collection of items - mutable - can have duplicates
+# Set - Unordered - Not indexed - mutable - no duplicates
+
+names_set = {'John', 'Jane', 'Stacy', 'Mike'}
+
+names_set.add('Jane')
+print(names_set)
+
+names_set.remove('John')
+print(names_set)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 3, 5, 7, 9}
+
+print(set1.union(set2))
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 3, 5, 7, 9}
+
+print(set1.intersection(set2))
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 3, 5, 7, 9}
+
+print(set1.difference(set2))
